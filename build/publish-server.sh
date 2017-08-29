e3d3e6dede35256b14f9d4dd04eb32a7002a01a0
Yao <yaozhou.wuhu@gmail.com>
2017-08-29 16:46:37 +0800

    update

--- build/publish-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cp -r /home/eric-zhou/develop/jekyll-blog/_site/* /home/eric-zhou/develop/yaozhou.github.io/
-cd /home/eric-zhou/develop/yaozhou.github.io
-rm *.sh
-git add .
-git commit -a -m "normal modify"
-git push origin master
--- build/start-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-jekyll build
-jekyll server
