65ea162436b5b4021d4a9a389385125b667f96c2
Yao <yaozhou.wuhu@gmail.com>
2017-08-29 16:54:01 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-BLOG_PATH='~/develop/yaozhou.github.io'
+BLOG_PATH=~/develop/yaozhou.github.io
 
 rm -rf $BLOG_PATH/*
-cp _site/* $BLOG_PATH/
+cp -r _site/* $BLOG_PATH/
 cd $BLOG_PATH/
 git add .
 git commit -m "update"
